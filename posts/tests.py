ca185fd31f0c605d2e583c4b8ae0b59fab99a14b
flavioMotaa <motaflavio145@gmail.com>
2021-04-26 20:58:52 -0300

    adding Post creation test

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.db import models
+from django.utils import timezone
+from .models import Post
 
 # Create your tests here.
+class PostTestCase(TestCase):
+    def createTestPost(self, topic="testTopic", title="something"):
+        return Post.objects.create(topic=topic, title=title, created_at=timezone.now())
+    
+    def test_IfPostCreated(self):
+        created_post = self.createTestPost()
+        self.assertTrue(isinstance(created_post, Post))
+        self.assertEqual(created_post.__str__(), created_post.title)
