75e290905bcd7d2880832588488df9a2aaf633f2
flavioMotaa <motaflavio145@gmail.com>
2021-04-27 08:20:56 -0300

    adding edit and delete functions to superusers

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from .models import Post
 from .forms import WritePost
 
@@ -16,7 +17,27 @@ def writePost(request):
             post.save()
             return redirect("/")
     return render(request, 'posts/addpost.html', {"form": escreverPost})
-
+@login_required
+def editPost(request, id):
+    if request.user.is_superuser:
+        post_alvo = get_object_or_404(Post, pk=id)
+        editarPost = WritePost(request.POST, instance=post_alvo)
+        if request.method == "POST":
+            if editarPost.is_valid():
+                post = editarPost.save(commit=False)
+                post.save()
+                return redirect("/")
+        return render(request, 'posts/editpost.html', {"editar": editarPost})
+    else:
+        return redirect("/")
+@login_required
+def deletePost(request, id):
+    if request.user.is_superuser:
+        post = get_object_or_404(Post, pk=id)
+        post.delete()
+        return redirect("/")
+    else:
+        return redirect("/")
 def readPost(request, id):
     post = get_object_or_404(Post, pk=id)
     return render(request, 'posts/postwritten.html', {"post": post, "id": id})
