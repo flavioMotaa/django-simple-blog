bad164e166d4fffa92af2d029155916d2d74997a
flavioMotaa <motaflavio145@gmail.com>
2021-04-27 07:41:24 -0300

    adding post saving test

--- posts/tests.py
+++ posts/tests.py
@@ -7,10 +7,10 @@ from .models import Post
 class PostTestCase(TestCase):
     def createTestPost(self, topic="testTopic", title="something"):
         return Post.objects.create(topic=topic, title=title, created_at=timezone.now())
+
     def test_savePost(self):
         postToBeSaved = self.createTestPost()
         self.assertEqual(postToBeSaved.save(), postToBeSaved) #Isso tem que dar erro
-    
     def test_IfPostCreated(self):
         created_post = self.createTestPost()
         self.assertTrue(isinstance(created_post, Post))
